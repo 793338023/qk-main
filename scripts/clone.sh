177872ad8303f60e0313ed5a94eaf3eb9d9eb50c
Mr.zhang <793338023@qq.com>
2022-11-20 17:49:58 +0800

    拉取

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -4,16 +4,17 @@ sub1='https://github.com/793338023/sub-vue.git'
 sub2='https://github.com/793338023/sub-react.git'
 sub3='https://github.com/793338023/sub-html.git'
 
-if [ ! -d sub-vue ]; then
-git clone $sub1
-fi
+subName=(sub-vue sub-react sub-html)
 
-if [ ! -d sub-react ]; then
-git clone $sub2
-fi
+subs=($sub1 $sub2 $sub3)
 
-if [ ! -d sub-html ]; then
-git clone $sub3
-fi
-
+for i in "${!subName[@]}"
+do  
+{
+    if [ ! -d ${subName[i]} ]; then
+        git clone ${subs[i]}
+    fi
+}&
+done
+wait
 
